096f0de8a9a48b5830793b1e9f6a9d17f408f0ed
Valentina Pezzi <valente.pezzi@gmail.com>
2022-05-27 15:56:09 +0200

    waves moving colors

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # canvas-hero
 
 Series of responsive fullscreen banners focusing on canvas.
+
+to do:
+- work on moving wave
+- supporting function for wave clip top & bottom ?
--- src/designs/waves.js
+++ src/designs/waves.js
@@ -11,67 +11,21 @@ function resize () {
   canvas.height = canvasH
 }
 
-function setRadius () {
+function getLonger() {
   let a;
-  (canvasW > canvasH) ? a = canvasW / 2 : a = canvasH / 2 
+  (canvasW > canvasH) ? a = canvasW : a = canvasH
   return a
 }
 
-function setVel() {
-  let v = c_random.range(0.5, 3)
-  return v
+function getGrd(rad) {
+  let circleGdr = ctx.createRadialGradient(0, 0, 0, 0, 0, rad)
+  circleGdr.addColorStop(0, 'rgb(210, 163, 93)')
+  circleGdr.addColorStop(0.30, 'rgb(217, 177, 118')
+  circleGdr.addColorStop(0.56, 'rgb(236, 213, 179')
+  circleGdr.addColorStop(0.90, 'rgb(201, 232, 249)')
+  circleGdr.addColorStop(1, 'rgb(220, 220, 220)')
+  return circleGdr
 }
-
-const colors = [
-  {
-    'r' : 210,
-    'b' : 163,
-    'g' : 93
-  },
-  {
-    'r' : 217,
-    'b' : 177,
-    'g' : 118
-  },
-  {
-    'r' : 236,
-    'b' : 213,
-    'g' : 179
-  },
-  {
-    'r' : 202,
-    'b' : 214,
-    'g' : 221
-  },
-  {
-    'r' : 220,
-    'b' : 220,
-    'g' : 220
-  }
-]
-
-class Circle{
-  constructor(r, b, g) {
-    this.rad = setRadius()
-    this.x = c_random.range(0, canvasW)
-    this.y = c_random.range(0, canvasH)
-    this.velX = setVel()
-    this.velY = setVel()
-    this.start = `rgb(${r}, ${b}, ${g}, 1)`
-    this.end = `rgb(${r}, ${b}, ${g}, 0)`
-  }
-}
-
-let points = []
-colors.map(item => { 
-  let p = new Circle(item.r, item.b, item.g)
-  points.push(p)
-})
-let shapePoints = []
-colors.map(item => { 
-  let p = new Circle(item.r, item.b, item.g)
-  shapePoints.push(p)
-})
 
 // SHAPE
 let topPath = [
@@ -80,140 +34,140 @@ let topPath = [
     'y' : 0.5 / 4 * canvasH
   },
   {
-    'x' : 1 / 3 * canvasW,
+    'x' : 0.5 / 2 * canvasW,
     'y' : 0.5 / 8 * canvasH
   },
   {
-    'x' : 1 /2 * canvasW,
-    'y' : 0.5/4 * canvasH
+    'x' : 1 / 2 * canvasW,
+    'y' : 0.5 / 4 * canvasH
   },
   {
-    'x' : 2/3 * canvasW,
-    'y' : 1.5/8 * canvasH
+    'x' : 1.5 / 2 * canvasW,
+    'y' : 1.5 / 8 * canvasH
   },
   {
     'x' : canvasW,
-    'y' : 0.5/4 * canvasH
+    'y' : 0.5 / 4 * canvasH
   }
 ]
 
 let bottomPath = [
   { 
-    'x' : canvasW,
-    'y' : 3.5/4 * canvasH
+    'x' : 0,
+    'y' : 3.5 / 4 * canvasH
   },
   { 
-    'x' : 2/3 * canvasW,
+    'x' : 0.5 / 2 * canvasW,
     'y' : 7.5/8 * canvasH
   },
   { 
-    'x' : 1/2 * canvasW,
+    'x' : 1 / 2 * canvasW,
     'y' : 3.5/4 * canvasH
   },
   { 
-    'x' : 1/3 * canvasW,
+    'x' : 1.5 / 2 * canvasW,
     'y' : 6.5/8 * canvasH
   },
   { 
-    'x' : 0,
-    'y' : 3.5/4 * canvasH
+    'x' : canvasW,
+    'y' : 3.5 / 4 * canvasH
   }
 ]
 
+// CIRCLES
+class Origin {
+  constructor() {
+    this.x = c_random.range(0, canvasW)
+    this.y = c_random.range(0, canvasH)
+    this.velX = c_random.range(0.5, 3)
+    this.velY = c_random.range(0.5, 3)
+  }
+}
+
+let topOrigin = new Origin()
+let bottomOrigin = new Origin()
+
 // TITLE
-const radius = 200
-let grd = ctx.createRadialGradient(
+const titleRad = 200
+let grdWhite = ctx.createRadialGradient(
   canvasW / 2, 
-  canvasH / 2 + radius, 
+  canvasH / 2 + titleRad, 
   1, 
   canvasW / 2, 
-  canvasH / 2 + radius,
-  radius * 2.5
+  canvasH / 2 + titleRad,
+  titleRad * 2.5
   )
-grd.addColorStop(0, 'rgb(255, 255, 255, 0)');
-grd.addColorStop(1, 'rgb(255, 255, 255, 1)')
+grdWhite.addColorStop(0, 'rgb(255, 255, 255, 0)')
+grdWhite.addColorStop(1, 'rgb(255, 255, 255, 1)')
 
 function waves () {
   resize()
   ctx.clearRect(0, 0, canvasW, canvasH)
-  
+  let circleRad = getLonger() / 1.75
+
+  ctx.fillStyle = getGrd(circleRad)
+  // BACKGROUND
   ctx.save()
-  ctx.fillStyle = 'rgb(217, 177, 118'
-  ctx.fillRect(0, 0, canvasW, canvasH)
+  ctx.translate(canvasW / 2, canvasH / 2)
+  
+  ctx.arc(0, 0, circleRad, 0, 2 * Math.PI)
+  ctx.fill()
   ctx.restore()
 
-  // MOVING POIS
-  points.map( (item) => {
-    if (item.x < -10 || item.x > canvasW +10) { //resize smaller
-      item.x = setX()
-      item.y = setY()
-    }
-    ctx.save()
-    ctx.translate(item.x, item.y)
-    let itemGrd = ctx.createRadialGradient(0, 0, 5, 0, 0, item.rad)
-    itemGrd.addColorStop(0, item.start)
-    itemGrd.addColorStop(1, item.end)
-    ctx.fillStyle = itemGrd
-    ctx.beginPath()
-    ctx.arc(0, 0, item.rad, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.restore()
-    if (item.x <= 0 || item.x >= canvasW) item.velX *= -1 //bounce
-    if (item.y <= 0 || item.y >= canvasH) item.velY *= -1
-    item.x += item.velX
-    item.y += item.velY
-  })
-  ctx.globalCompositeOperation = 'color'
-
-  // ctx.globalCompositeOperation = 'source-atop'
-  // SHAPE
-  // do not save-restore or it won't clip the elements after
-  ctx.strokeStyle = 'rgb(0, 0, 0, 0)'
+  // TOP
+  ctx.save()
   ctx.beginPath()
   ctx.moveTo(topPath[0].x, topPath[0].y)
-  // TOP
   ctx.bezierCurveTo(topPath[1].x, topPath[1].y, topPath[1].x, topPath[1].y, topPath[2].x, topPath[2].y)
   ctx.bezierCurveTo(topPath[3].x, topPath[3].y, topPath[3].x, topPath[3].y, topPath[4].x, topPath[4].y)
-  // BOTTOM
-  ctx.lineTo(bottomPath[0].x, bottomPath[0].y)
-  ctx.bezierCurveTo(bottomPath[1].x, bottomPath[1].y, bottomPath[1].x, bottomPath[1].y, bottomPath[2].x, bottomPath[2].y)
-  ctx.bezierCurveTo(bottomPath[3].x, bottomPath[3].y, bottomPath[3].x, bottomPath[3].y, bottomPath[4].x, bottomPath[4].y)
+  ctx.lineTo(canvasW, 0)
+  ctx.lineTo(0, 0)
   ctx.lineTo(topPath[0].x, topPath[0].y)
   ctx.closePath()
-  ctx.stroke()
-  ctx.clip();
-  // move shape WORK ON THIS
-  (topPath[1].y <= 0.5 / 8 * canvasH) ? topPath[1].y = topPath[1].y + 1: topPath[1].y = topPath[1].y -1
+  ctx.clip()
+  ctx.translate(topOrigin.x, topOrigin.y)
+  ctx.fillRect(0, 0, canvasW, canvasH)
+  ctx.fill()
+  ctx.restore()
+  // move
+  if (topOrigin.x <= 0 || topOrigin.x >= canvasW) topOrigin.velX *= -1 //bounce
+  if (topOrigin.y <= 0 || topOrigin.y >= canvasH) topOrigin.velY *= -1
+  topOrigin.x += topOrigin.velX
+  topOrigin.y += topOrigin.velY;  
+  // if (topPath[1].y >= 0.5 / 8 * canvasH & topPath[1].y <= 1.5 / 8 * canvasH) {
+  //   topPath[1].y = topPath[1].y + 0.5
+  // } else {
+  //   topPath[1].y = topPath[1].y - 0.5
+  // }
 
-  // MOVING POIS 2
-  shapePoints.map( (item) => {
-    if (item.x < -10 || item.x > canvasW +10) { //resize smaller
-      item.x = setX()
-      item.y = setY()
-    }
-    ctx.save()
-    ctx.translate(item.x, item.y)
-    let itemGrd = ctx.createRadialGradient(0, 0, 5, 0, 0, item.rad)
-    itemGrd.addColorStop(0, item.start)
-    itemGrd.addColorStop(1, item.end)
-    ctx.fillStyle = itemGrd
-    ctx.beginPath()
-    ctx.arc(0, 0, item.rad, 0, Math.PI * 2)
-    ctx.fill()
-    ctx.restore()
-    if (item.x <= 0 || item.x >= canvasW) item.velX *= -1 //bounce
-    if (item.y <= 0 || item.y >= canvasH) item.velY *= -1
-    item.x += item.velX
-    item.y += item.velY
-  })
-  ctx.globalCompositeOperation = 'color'
+  // BOTTOM
+  ctx.save()
+  ctx.beginPath()
+  ctx.moveTo(bottomPath[0].x, bottomPath[0].y)
+  ctx.bezierCurveTo(bottomPath[1].x, bottomPath[1].y, bottomPath[1].x, bottomPath[1].y, bottomPath[2].x, bottomPath[2].y)
+  ctx.bezierCurveTo(bottomPath[3].x, bottomPath[3].y, bottomPath[3].x, bottomPath[3].y, bottomPath[4].x, bottomPath[4].y)
+  ctx.lineTo(canvasW, canvasH)
+  ctx.lineTo(0, canvasH)
+  ctx.lineTo(bottomPath[0].x, bottomPath[0].y)
+  ctx.closePath()
+  ctx.clip()
+  // move ctx start
+  ctx.translate(bottomOrigin.x, bottomOrigin.y)
+  if (bottomOrigin.x <= 0 || bottomOrigin.x >= canvasW) bottomOrigin.velX *= -1 //bounce
+  if (bottomOrigin.y <= 0 || bottomOrigin.y >= canvasH) bottomOrigin.velY *= -1
+  bottomOrigin.x += bottomOrigin.velX
+  bottomOrigin.y += bottomOrigin.velY
+  // fill clip
+  ctx.fillRect(0, 0, canvasW, canvasH)
+  ctx.fill()
+  ctx.restore()
 
   // CIRCLE
-  ctx.globalCompositeOperation = 'source-atop'
+  // ctx.globalCompositeOperation = 'source-atop'
   ctx.save()
-  ctx.fillStyle = grd
+  ctx.fillStyle = grdWhite
   ctx.beginPath()
-  ctx.arc(canvasW / 2, canvasH / 2, radius, 0, 2 * Math.PI)
+  ctx.arc(canvasW / 2, canvasH / 2, titleRad, 0, 2 * Math.PI)
   ctx.fill()
   ctx.restore()
 
